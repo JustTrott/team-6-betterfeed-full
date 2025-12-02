66e1911efe4ad66c06c28c1216092f9aaf8e505a
Robbie Liesegang <rbl9656@nyu.edu>
2025-12-02 16:16:20 -0500

    Streamlined engagement bar

--- components/feed/EngagementBar.tsx
+++ components/feed/EngagementBar.tsx
@@ -4,13 +4,12 @@ import { cn } from '../../lib/utils'
 interface EngagementBarProps {
   likes: number
   saves: number
-  views: number
   onLike: () => void
   onSave: () => void
   active: { like: boolean; save: boolean }
 }
 
-export const EngagementBar = ({ likes, saves, views, onLike, onSave, active }: EngagementBarProps) => {
+export const EngagementBar = ({ likes, saves, onLike, onSave, active }: EngagementBarProps) => {
   return (
     <div className="bf-engagement-bar">
       <button
@@ -35,11 +34,6 @@ export const EngagementBar = ({ likes, saves, views, onLike, onSave, active }: E
         <Bookmark className={cn('bf-icon-sm', active.save && 'is-active')} />
         <span className="bf-engagement-bar__value">{saves}</span>
       </button>
-      <div className="bf-engagement-bar__views">
-        <Eye className="bf-icon-sm" />
-        <span className="bf-engagement-bar__value">{views}</span>
-      </div>
     </div>
   )
 }
-
